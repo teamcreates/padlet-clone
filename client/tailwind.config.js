e6d2a6a74a1896ea683d90f7c5f52db47dfe079a
D <61399248+BotGUyz@users.noreply.github.com>
2022-06-01 19:56:19 +0700

    headbar update
    headbar created

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  content: [],
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    // ...
+  }
+  // ...
 }
