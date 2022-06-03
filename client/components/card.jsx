2b6f1e7da33af7eb2b5b491b6ae866e29a7bdf6d
D <61399248+BotGUyz@users.noreply.github.com>
2022-06-03 18:01:14 +0700

    add system made
    yes

--- client/components/card.jsx
+++ client/components/card.jsx
@@ -3,13 +3,13 @@ import "tailwindcss/tailwind.css";
 
 export default function Card(props) {
     return(
-        <div className='rounded-lg bg-white ph-2 px-2 pb-2 m-5 cardc truncate'>
-            <p className='m-10 break-words'>{props.content}</p>
+        <div className='rounded-lg bg-white cardc break-words ustify-center items-center h-screen'>
+            <p className=' m-1'>{props.content}</p>
             <style jsx>{`
           .cardc {
-            max-width: 40rem;
-            min-width: 20rem;
-            text-align: center;
+            max-width: 20rem;
+            min-width: 15rem;
+            margin: 0.3rem;
           }
       `}</style>
         </div>
